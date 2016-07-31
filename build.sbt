515e340c6bbe3107a01fc86e1eeb5af0a743fa93
Kai Chen <kai@sorrentocorp.com>
2016-07-31 14:29:43 -0700

    feat(check code is sequential or error)

--- build.sbt
+++ build.sbt
@@ -1,9 +1,8 @@
 name := "scala-macros"
 organization := "com.sorrentocorp"
-version := "0.1-SNAPSHOT"
 licenses += ("MIT", url("http://opensource.org/licenses/MIT"))
 scalaVersion := "2.11.8"
 libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
 publishTo := Some("Bintray API Realm" at
-  "https://api.bintray.com/content/kai-chen/maven/com.sorrentocorp.scala-macros/" + version.value)
+  "https://api.bintray.com/content/kai-chen/maven/com.sorrentocorp.scala-macros/" + version.value + ";publish=1")
 credentials += Credentials(Path.userHome / ".bintray" / ".credentials")
--- src/main/scala/com/sorrentocorp/macros/Macros.scala
+++ src/main/scala/com/sorrentocorp/macros/Macros.scala
@@ -1,5 +1,6 @@
 package com.sorrentocorp.macros
 
+import scala.concurrent.Future
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox.Context
 
@@ -7,17 +8,20 @@ object CheckWhenCompile {
   def sequentially[A, B](body: A => B): A => B = macro seqImpl[A, B]
   def seqImpl[A: c.WeakTypeTag, B: c.WeakTypeTag](c: Context)(body: c.Expr[A => B]): c.Expr[A => B] = {
     import c.universe._
-    c.info(c.enclosingPosition, "Checking body " + body.tree, true)
+
+    def check(tree: Tree): Unit =
+      if (tree.tpe <:< typeOf[Future[_]])
+        c.error(c.enclosingPosition, s"line contains asynchronous code; it shouldn't be in a transactional block")
 
     body.tree match {
       case Function(_, Block(stats, expr)) =>
-        c.info(c.enclosingPosition, "matched function", false)
-        c.info(c.enclosingPosition, "stats " + stats, true)
-        c.info(c.enclosingPosition, "expr " + expr, true)
-      case _ =>
-        c.warning(c.enclosingPosition, "not matched")
-        c.warning(c.enclosingPosition, body.tree.toString)
+        stats foreach check
+        check(expr)
+
+      case x@_ =>
+        c.error(c.enclosingPosition, s"Expected an anomynous function, but found $x")
     }
+
     body
   }
 }
